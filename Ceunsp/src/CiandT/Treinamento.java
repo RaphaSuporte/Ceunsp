1f7d429af97f894b3aa87a47599a00e89543a6a4
Raphael Andrdrade <rapha.suporte@gmail.com>
2019-02-14 14:34:29 -0200

    Exemplo de Entrada de usuario

---
+++ Ceunsp/src/CiandT/Treinamento.java
@@ -0,0 +1,20 @@
+package CiandT;
+
+import java.util.Scanner;
+public class Treinamento {
+
+	public static void main(String[] args) {
+		
+	int x;
+	int resultado;
+	
+	Scanner entrada = new Scanner (System.in);
+	System.out.println("Digite o Numero " );
+	x = entrada.nextInt();
+	
+	System.out.println("Resultado é " + x * 5);
+	
+	
+	}
+
+}
